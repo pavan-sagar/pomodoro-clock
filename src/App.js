d8ecfbd99ea3bf7ee5f01d4ab6e50137ac263056
pavan-sagar <pavan.sagar@outlook.com>
2020-06-10 21:40:30 +0530

    Initial basic layout created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      breakMin: 5,
+      sessionMin: 25,
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="pomodoro-container">
+          <h1>Pomodoro Clock</h1>
+          <div className="break-container">
+            <h2 className="break-header">Break Length</h2>
+            <i class="fa fa-arrow-down fa-2x" />
+            <p className="break-minutes">{this.state.breakMin}</p>
+            <i class="fa fa-arrow-up fa-2x" />
+          </div>
+
+          <div className="session-container">
+            <h2 className="session-header">Session Length</h2>
+            <i class="fa fa-arrow-down fa-2x" />
+            <p className="session-minutes">{this.state.sessionMin}</p>
+            <i class="fa fa-arrow-up fa-2x" />
+          </div>
+          <h2 className="session-timer-header">Session</h2>
+          <div className="button-bar">
+          <i class="fa fa-play fa-2x" />
+          <i class="fa fa-pause fa-2x" />
+          <i class="fa fa-refresh fa-2x" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
