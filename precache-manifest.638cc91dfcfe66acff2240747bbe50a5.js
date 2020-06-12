ef97e82e91852a62019a3714e3d820eb782acb79
pavan-sagar <pavan.sagar@outlook.com>
2020-06-12 22:06:21 +0530

    Updates

---
+++ precache-manifest.638cc91dfcfe66acff2240747bbe50a5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "111360acc3ff3397eee5da3aacff28c6",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "97b5c2538143546a5d3f",
+    "url": "/pomodoro-clock/static/css/2.8cd09bda.chunk.css"
+  },
+  {
+    "revision": "d144f371600fde9d837f",
+    "url": "/pomodoro-clock/static/css/main.8d287e07.chunk.css"
+  },
+  {
+    "revision": "97b5c2538143546a5d3f",
+    "url": "/pomodoro-clock/static/js/2.644bbe8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.644bbe8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d144f371600fde9d837f",
+    "url": "/pomodoro-clock/static/js/main.af2ad81d.chunk.js"
+  },
+  {
+    "revision": "a4dcdb46a55158e82e38",
+    "url": "/pomodoro-clock/static/js/runtime-main.6695a290.js"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/pomodoro-clock/static/media/digital-7.1e670d88.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/pomodoro-clock/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/pomodoro-clock/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/pomodoro-clock/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/pomodoro-clock/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/pomodoro-clock/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pomodoro-clock/static/media/logo.5d5d9eef.svg"
+  }
+]);
